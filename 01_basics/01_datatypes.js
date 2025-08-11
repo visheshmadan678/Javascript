564a5dccafe2b184b059ca41a58eed769056ae1d
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-11 09:54:31 +0000

    javascrpit datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // this will treat all of my code as newer version of java script 
+// alert(3+3) cannot use here because we are using node js not browser
+
+//data types in java script
+
+//number
+//big int
+//string
+//boolean
+//undefined
+//null //but treated as an object
+
+console.table([
+    typeof null,
+    typeof undefined
+])
