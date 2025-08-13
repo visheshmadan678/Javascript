b051c40471845068514c9f98cd366fc0fd009919
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-13 14:54:52 +0000

    conversion operation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,30 @@
+//this time we will learn the conversion operation in java script 
+
+let age = undefined; // in case of null it will store the 0 after conversion
+//if i will store the string and convert it to the number it will still show NaN 
+// and in case of undefined it will store nan as the ouput
+let nam = "vishu badmash";
+
+let age1 = 3000;
+let value = false;
+console.log(typeof age); // this will show that the type is number
+console.log(typeof nam); // this will show that the type if string
+console.log(typeof value); // this will show that type is boolean of our data type
+
+let val = Number(age); // if we want to change the data type just add the data type which we want to cahnge
+
+
+// let new_age = String(age1);
+
+// console.log(typeof(new_age));
+// console.log(typeof(val));
+// console.log(val); 
+
+// console.log("1" + 1);
+// console.log(1 + 1 + "1");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
