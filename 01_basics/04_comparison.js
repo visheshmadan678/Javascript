abb22c165945be77bd8bac53cc4e0d58c5c03e28
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-13 15:01:49 +0000

    comparison in javascript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1);
+console.log(1>=1);
+
+console.log(9>=1);
+
+console.log(null > 1); // null will remain null in this case 
+console.log(null == 1); // null will remain null in this case 
+console.log(null>=0); // this will convert the null to a value that's it is returning a true 
+
+
+console.log("2"===1); // this will also check for the data type 
+
