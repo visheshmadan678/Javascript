3fec8110e2af2a0d233629e718921d60fe7d7328
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-20 14:37:29 +0000

    timepass

---
+++ 03_basics/04_IIEF.js
@@ -0,0 +1,14 @@
+//immediately invoked functions expressions in java script 
+//today we will learn this 
+
+// IIFE NOT IIEF 
+//why to use iife   kyuki global scope ke variables se kabhi kabhi problem hoti hai or function pollute ho skta hai 
+//that's why we use iife
+
+
+//1st bracket for function definition 
+//2nd bracket for exectuion 
+
+(()=>{
+console.log("vishubadmash was a good girl");
+})(); //use a semicolon to tell the compiler that i am immediated succesfully and also executed successfully 
